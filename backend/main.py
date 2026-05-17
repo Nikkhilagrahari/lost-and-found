ee88c2ee94a5cf696456372b94d886aad8b77300
Nikkhilagrahari <nikkhil.agrahari1977@gmail.com>
2026-05-17 23:14:25 +0530

    add vercel cors

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://lost-and-found-qd1b.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
